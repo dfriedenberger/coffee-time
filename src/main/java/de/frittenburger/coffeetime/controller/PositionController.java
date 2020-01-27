debdef495745217e9a23f2cb5348b96ebe160f68
DirkFriedenberger <DirkFriedenberger@deutschebahn.com>
2020-01-27 23:10:10 +0100

    fix build

--- src/main/java/de/frittenburger/coffeetime/controller/PositionController.java
+++ src/main/java/de/frittenburger/coffeetime/controller/PositionController.java
@@ -17,11 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 
+
 import de.frittenburger.api.PositionApi;
 import de.frittenburger.cqrs.impl.LocationQueryServiceImpl;
 import de.frittenburger.cqrs.interfaces.LocationQueryService;
 import de.frittenburger.cqrs.model.LocationEvent;
-import de.frittenburger.model.Location;
+import de.frittenburger.model.Position;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
@@ -34,12 +35,12 @@ public class PositionController implements PositionApi {
     private static final LocationQueryService queryService = new LocationQueryServiceImpl(new File("data"));
 
     @Override
-    public ResponseEntity<Location> currentPosition(String id) {
+    public ResponseEntity<Position> currentPosition(String id) {
     	
     	LocationEvent evnt = queryService.getCurrentLocation(id);
     	if(evnt == null)
 			return new ResponseEntity<>( HttpStatus.NOT_FOUND );
-    	Location location = new Location();
+    	Position location = new Position();
     	
     	location.setLatitude(evnt.getLatitude());
     	location.setLongitude(evnt.getLongitude());
