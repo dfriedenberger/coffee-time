b6af95f28f8a0adac66d62e254918a7d80652ac8
DirkFriedenberger <DirkFriedenberger@deutschebahn.com>
2020-01-27 22:48:28 +0100

    add map

--- src/main/java/de/frittenburger/coffeetime/controller/PageController.java
+++ src/main/java/de/frittenburger/coffeetime/controller/PageController.java
@@ -1,7 +1,11 @@
 package de.frittenburger.coffeetime.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.logging.log4j.LogManager;
@@ -9,6 +13,9 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 
@@ -18,7 +25,16 @@ public class PageController {
  
 	private static final Logger logger = LogManager.getLogger(PageController.class);
 
+	private Map<String, String> map = null;
 	
+	@PostConstruct
+	public void init() {
+		try {
+			map = new ObjectMapper().readValue(new File("config/app.json"), new TypeReference<HashMap<String, String>>() {});
+		} catch (IOException e) {
+			logger.error(e);
+		}
+	}
 	
 	@RequestMapping("/")
 	public String welcome(@RequestHeader Map<String, String> headers, Map<String, Object> model, HttpServletRequest request) {
@@ -27,9 +43,10 @@ public class PageController {
 	    	logger.info(String.format("Header '%s' = %s", key, value));
 	    });
 	    
-	    model.put("header", "Spring Boot Vorlage");
-	    model.put("message", "Herzlich Willkommen.");
-	    
+	    model.put("header", "Wo ist Dirk?");
+	    model.put("deviceId", "DF");
+		model.put("mapboxAccessToken", map.get("mapboxAccessToken"));
+
 		return "welcome";
 	}
 	
